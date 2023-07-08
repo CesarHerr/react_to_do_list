c78d667c056baa817987f6e4b4385af726b22b4b
Cesar Herrera <cesarherr321@gmail.com>
2023-07-07 22:36:34 -0400

    Add icons to the project

--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -1,5 +1,8 @@
 import { useState } from 'react';
 import styles from '../styles/TodoItem.module.css';
+import { FaTrash } from "react-icons/fa";
+import { AiFillEdit } from "react-icons/ai";
+
 
 const TodoItem = ({ itemProp, handleChange, delTodo, setUpdate }) => {
   const [editing, setEditing] = useState(false);
@@ -37,8 +40,12 @@ const TodoItem = ({ itemProp, handleChange, delTodo, setUpdate }) => {
           checked={itemProp.completed}
           onChange={() => handleChange(itemProp.id)}
         />
-        <button onClick={handleEditing}>Edit</button>
-        <button onClick={() => delTodo(itemProp.id)}>Delete</button>
+        <button onClick={handleEditing}>
+        <AiFillEdit style={{ color: "#5e5e5e", fontSize: "16px" }} />
+        </button>
+        <button onClick={() => delTodo(itemProp.id)}>
+        <FaTrash style={{ color: "#5e5e5e", fontSize: "16px" }} />
+        </button>
         <span style={itemProp.completed ? completedStyle : null}>
           {itemProp.title}
         </span>
